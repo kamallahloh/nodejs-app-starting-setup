225b7253db19b76ecbfa01670bb4dd232e5ed40c
Kamal Lahloh <kamal.lahloh@gmail.com>
2025-04-03 09:01:53 +0300

    docker

--- assignment-problem/node-app/server.js
+++ assignment-problem/node-app/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
@@ -8,4 +8,14 @@ app.get('/', (req, res) => {
   `);
 })
 
+// local machine node -v = v14.21.3
+console.log(`Running on Node.js version: ${process.version}`);
+// docker Running on Node.js version: v23.9.0 as per Dockerfile and latest version of node
+
 app.listen(3000);
+
+
+/*(node:1) [MODULE_TYPELESS_PACKAGE_JSON] Warning: Module type of file:///app/server.js is not specified and it doesn't parse as CommonJS.
+Reparsing as ES module because module syntax was detected. This incurs a performance overhead.
+To eliminate this warning, add "type": "module" to /app/package.json.
+(Use `node --trace-warnings ...` to show where the warning was created)*/
